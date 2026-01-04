ede4727736f386a29b8e39950ea871948c8c996a
astralarchitect <154975712+AstralArchitect@users.noreply.github.com>
2026-01-04 18:00:11 +0100

    update

--- app/src/main/java/fr/matthstudio/themeteo/forecastViewer/DayGraphsActivity.kt
+++ app/src/main/java/fr/matthstudio/themeteo/forecastViewer/DayGraphsActivity.kt
@@ -185,7 +185,7 @@ fun GraphsScreen(viewModel: WeatherViewModel, startDateTime: LocalDateTime) {
                 GenericGraph(
                     viewModel,
                     GraphType.TEMP,
-                    Color(0xFFFFD54F),
+                    Color(0xFFFFF176),
                     scrollState = scrollState
                 )
                 if (viewModel.hourlyForecast.collectAsState().value.first().apparentTemperature != null) {
@@ -194,7 +194,7 @@ fun GraphsScreen(viewModel: WeatherViewModel, startDateTime: LocalDateTime) {
                     GenericGraph(
                         viewModel,
                         GraphType.A_TEMP,
-                        Color(0xFFFFF176),
+                        Color(0xFFFFD54F),
                         scrollState = scrollState
                     )
                 }
